a89afbfbfa25c7d5dcc4be9ac1d27043476ecdfc
devmarcs <devmarcelo.gus@gmail.com>
2025-03-02 20:48:27 -0300

    Adicionando o admin para os devedores

--- pagme/admin.py
+++ pagme/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from .models import Expense
+from .models import Expense, Debtors
 
 
 class ExpanseAdmin(admin.ModelAdmin):
     list_display = [
         "value",
         "days",
+        "debtor",
+    ]
+
+class DebitorsAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "nickname",
     ]
 
 admin.site.register(Expense, ExpanseAdmin)
+admin.site.register(Debtors, DebitorsAdmin)
