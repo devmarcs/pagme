c2fe6cb81543e4a41a44c5db9baa5d9557b79247
devmarcs <devmarcelo.gus@gmail.com>
2025-03-03 11:50:50 -0300

    Retirando as rotas de autenticação

--- project_root/urls.py
+++ project_root/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("pagme.urls"), name="pagme"),
-    path("", include("accounts.urls"), name="accounts"),
-    path("accounts/", include('django.contrib.auth.urls')),
+    #path("", include("accounts.urls"), name="accounts"),
+    #path("accounts/", include('django.contrib.auth.urls')),
 ]
